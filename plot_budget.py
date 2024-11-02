d40505345b4b0865432bbe36e0526b9406aff6d1
Matthew Teelucksingh <teelucksingh.matthew1@gmail.com>
2024-11-02 03:22:42 -0700

    Adding export

--- plot_budget.py
+++ plot_budget.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("csv_path")
     parser.add_argument("-p", "--plot", action="store_true", help="")
+    parser.add_argument("-o", "--output", help="If the generated plot is to be output as a static file")
 
     args = parser.parse_args()
 
@@ -23,7 +24,13 @@ if __name__ == "__main__":
 
     print(df)
 
+    fig = px.area(df, x=df.index, y=["balance", "income", "expenses"])
+    fig.update_layout(title={'text': '<b>Financials</b>'})
+
+    if args.output:
+        fig.write_html(args.output)
+
     if args.plot:
-        fig = px.area(df, x=df.index, y=["balance", "income", "expenses"])
-        fig.update_layout(title={'text': '<b>Financials</b>'})
         fig.show()
+
+            
